6304a85ec53995031751d5315fc5c510c251b74d
wangenqi <86047805@qq.com>
2025-10-19 19:47:10 +0800

    更改了接口和控制器方法的位置，完成了定义领域模型和接口的任务

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/BatchQueryApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/BatchQueryApis.java
@@ -1,5 +1,10 @@
 package com.zeroone.star.project.j2.store;
 
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j2.store.BatchListDTO;
+import com.zeroone.star.project.query.j2.store.BatchQuery;
+import com.zeroone.star.project.vo.JsonVO;
+
 /**
  * @BelongsProject: psi-java
  * @BelongsPackage: com.zeroone.star.project.j2.store
@@ -9,4 +14,10 @@ package com.zeroone.star.project.j2.store;
  * @Version: 1.0
  */
 public interface BatchQueryApis {
+    /**
+     * 查询批次列表数据
+     * @param query
+     * @return 批次列表数据
+     */
+    JsonVO<PageDTO<BatchListDTO>> listBatch(BatchQuery query);
 }
