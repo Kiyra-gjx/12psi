6887e587404edd4507a797aa72d3562b9654d155
Carry <1813491989@qq.com>
2025-10-19 13:53:50 +0800

    Carry新增批次列表查询领域模型

---
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/CostDTO.java
@@ -0,0 +1,17 @@
+package com.zeroone.star.project.dto.j2.store;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+/**
+ * @BelongsProject: psi-java
+ * @BelongsPackage: com.zeroone.star.project.dto.j2.store
+ * @Author: 高
+ * @CreateTime: 2025-10-19 13:52
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@Data
+@ApiModel("")
+public class CostDTO {
+}
