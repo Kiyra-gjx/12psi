55895cd0a4e974ae6a073998163ec773c615253a
Carry <1813491989@qq.com>
2025-10-19 13:47:18 +0800

    Carry新增商品详情查询对象

---
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/query/j2/store/InventoryDetailQuery.java
@@ -0,0 +1,17 @@
+package com.zeroone.star.project.query.j2.store;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+/**
+ * @BelongsProject: psi-java
+ * @BelongsPackage: com.zeroone.star.project.query.j2.store
+ * @Author: 高
+ * @CreateTime: 2025-10-19 13:46
+ * @Description: 商品详情查询对象
+ * @Version: 1.0
+ */
+@Data
+@ApiModel("商品详情查询对象")
+public class InventoryDetailQuery {
+}
