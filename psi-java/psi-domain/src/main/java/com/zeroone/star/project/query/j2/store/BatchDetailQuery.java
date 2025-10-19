04ee195fae9f8f9243d9661670864140b4df08d9
Carry <1813491989@qq.com>
2025-10-19 10:41:54 +0800

    Carry新增获取指定批次详情参数对象

---
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/query/j2/store/BatchDetailQuery.java
@@ -0,0 +1,17 @@
+package com.zeroone.star.project.query.j2.store;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+/**
+ * @BelongsProject: psi-java
+ * @BelongsPackage: com.zeroone.star.project.query.j2.store
+ * @Author: 高
+ * @CreateTime: 2025-10-19 10:41
+ * @Description: 批次查询详情参数对象
+ * @Version: 1.0
+ */
+@Data
+@ApiModel("批次查询详情参数")
+public class BatchDetailQuery {
+}
