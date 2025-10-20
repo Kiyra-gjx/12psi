f1bf593282bfca21ceca8019b3c8d4d9d161d1a5
shark <ni8428105liang@163.com>
2025-10-20 14:46:52 +0800

    增加其他入库单数据信息对象、其他出库单数据对象，其他出库单信息数据信息对象。

--- psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/OtherInListInfoDTO.java
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/OtherInListInfoDTO.java
@@ -13,7 +13,7 @@ import lombok.Data;
  * @Version: 1.0
  */
 @Data
-@ApiModel("")
+@ApiModel("其他入库单数据信息对象")
 public class OtherInListInfoDTO {
     @ApiModelProperty(value = "id", example = "1")
     int id;
