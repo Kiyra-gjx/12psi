c85e068db57959b521bbc62738c934ae764058f0
Carry <1813491989@qq.com>
2025-10-20 21:28:11 +0800

    Carry修改注解以及删除无用文件

--- psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/AttrStockDTO.java
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/AttrStockDTO.java
@@ -1,7 +1,7 @@
 package com.zeroone.star.project.dto.j2.store;
 
 
-import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Data
-@Api("商品属性库存对象")
+@ApiModel("商品属性库存对象")
 public class AttrStockDTO {
 
     @ApiModelProperty(value = "商品属性ID（内部使用）", example = "1")
