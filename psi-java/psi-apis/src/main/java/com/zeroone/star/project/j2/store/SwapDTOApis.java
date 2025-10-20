9a44dc699fcee7e24febf36b6091593271568539
xiaobuhuichihuoguo <1136503952@qq.com>
2025-10-21 00:11:54 +0800

    新增调拨单控制层

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/SwapDTOApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/SwapDTOApis.java
@@ -1,5 +1,6 @@
 package com.zeroone.star.project.j2.store;
 
+import com.zeroone.star.project.dto.j2.store.ShopListDto;
 import com.zeroone.star.project.dto.j2.store.SwapListDTO;
 import com.zeroone.star.project.query.j2.store.SwapQuery;
 import com.zeroone.star.project.query.j2.store.SwapQueryCondition;
@@ -8,8 +9,8 @@ import com.zeroone.star.project.vo.JsonVO;
 import java.util.List;
 
 /**
- * 调拨单详细信息接口
- * 定义了与调拨单详细列表相关的API操作
+ * 调拨单接口
+ * 定义了与调拨单，调拨单详细信息，列表相关的API操作
  */
 public interface SwapDTOApis {
     /**
@@ -28,8 +29,8 @@ public interface SwapDTOApis {
 
     /**
      * 新增调拨单
-     * @param swapQuery 调拨单列表
-     * @return 获取调拨单详细
+     * @param shopListDto 商品列表
+     * @return 包含调拨单详细信息的JsonVO对象
      */
-    JsonVO<String> addSwapList(SwapQuery swapQuery);
+    JsonVO<String> addSwapList(List<ShopListDto> shopListDto);
 }
