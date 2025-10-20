39d1feebb2904475aeb095518a20839a8089530d
wl <2064525277@qq.com>
2025-10-20 16:08:19 +0800

    提交分支

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/OtherOutApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/OtherOutApis.java
@@ -1,5 +1,10 @@
 package com.zeroone.star.project.j2.store;
 
+import com.zeroone.star.project.dto.j2.store.OtherOutListDTO;
+import com.zeroone.star.project.vo.JsonVO;
+
+import java.util.List;
+
 /**
  * @BelongsProject: psi-java
  * @BelongsPackage: com.zeroone.star.project.j2.store
@@ -9,4 +14,22 @@ package com.zeroone.star.project.j2.store;
  * @Version: 1.0
  */
 public interface OtherOutApis {
+    /**
+     * 修改其他出库单
+     * @param otherOutListDTO 修改参数
+     * @return 修改结果
+     */
+    JsonVO<String> updateOtherOutList(OtherOutListDTO otherOutListDTO);
+
+
+    /**
+     * 删除其他出库单
+     * @param ids 删除的出库单ID列表
+     * @return 删除结果
+     */
+    JsonVO<String> deleteOtherOutList(List<Integer> ids);
+
+
+
+
 }
