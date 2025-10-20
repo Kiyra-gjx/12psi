06bff59d80601cfa39d81d60580fffd12d11ea4a
guangNian <guangNian@email.com>
2025-10-20 17:57:28 +0800

    库存盘点接口以及领域模型的定义完成

--- psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/InventoryVerifyApis.java
+++ psi-java/psi-apis/src/main/java/com/zeroone/star/project/j2/store/InventoryVerifyApis.java
@@ -1,5 +1,11 @@
 package com.zeroone.star.project.j2.store;
 
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j2.store.BatchListDTO;
+import com.zeroone.star.project.dto.j2.store.InventoryVerifyListDTO;
+import com.zeroone.star.project.query.j2.store.InventoryVerifyQuery;
+import com.zeroone.star.project.vo.JsonVO;
+
 /**
  * @BelongsProject: psi-java
  * @BelongsPackage: com.zeroone.star.project.j2.store
@@ -9,4 +15,38 @@ package com.zeroone.star.project.j2.store;
  * @Version: 1.0
  */
 public interface InventoryVerifyApis {
+
+    /**
+     * 查询盘存库点数据
+     * @param query 查询参数
+     * @return  库存盘点数据
+     */
+    JsonVO<PageDTO<InventoryVerifyListDTO>> listInventoryVerify(InventoryVerifyQuery query);
+
+    /**
+     * 查询商品类别
+     * @return 商品类别数据
+     *
+     */
+    JsonVO<String> listGoodsCategory();
+
+    /**
+     * 查询商品品牌
+     * @return 商品品牌数据
+     */
+    JsonVO<String> listGoodsBrand();
+
+    /**
+     * 查询仓库信息
+     * @return 仓库信息数据
+     */
+    JsonVO<String> listWarehouse();
+
+    /**
+     * 导出库存盘点单
+     * @param
+     * @return 库存盘点单excel
+     */
+    JsonVO<byte[]> exportInventoryVerifyExcel();
+
 }
--- psi-java/psi-j2-storemanagement/src/main/java/com/zeroone/star/storemanagement/controller/InventoryVerifyController.java
+++ psi-java/psi-j2-storemanagement/src/main/java/com/zeroone/star/storemanagement/controller/InventoryVerifyController.java
@@ -1,7 +1,14 @@
 package com.zeroone.star.storemanagement.controller;
 
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j2.store.BatchListDTO;
+import com.zeroone.star.project.dto.j2.store.InventoryVerifyListDTO;
 import com.zeroone.star.project.j2.store.InventoryVerifyApis;
+import com.zeroone.star.project.query.j2.store.InventoryVerifyQuery;
+import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +24,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/verify")
 @Api(tags = "库存盘点相关接口")
 public class InventoryVerifyController implements InventoryVerifyApis {
+
+
+    @GetMapping("/list")
+    @ApiOperation(value = "查询库存盘点列表")
+    @Override
+    public JsonVO<PageDTO<InventoryVerifyListDTO>> listInventoryVerify(InventoryVerifyQuery query) {
+        return null;
+    }
+    @GetMapping("/goods/category")
+    @ApiOperation(value = "查询商品分类")
+    @Override
+    public JsonVO<String> listGoodsCategory() {
+        return null;
+    }
+
+    @GetMapping("/goods/brand")
+    @ApiOperation(value = "查询商品品牌")
+    @Override
+    public JsonVO<String> listGoodsBrand() {
+        return null;
+    }
+
+    @GetMapping("/warehouse")
+    @ApiOperation(value = "查询仓库信息")
+    @Override
+    public JsonVO<String> listWarehouse() {
+        return null;
+    }
+
+    @GetMapping("/export")
+    @ApiOperation(value = "导出库存盘点数据Excel")
+    @Override
+    public JsonVO<byte[]> exportInventoryVerifyExcel() {
+        return null;
+    }
 }
