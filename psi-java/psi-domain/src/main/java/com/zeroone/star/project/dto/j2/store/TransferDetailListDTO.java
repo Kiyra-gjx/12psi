6f85692a532cb77f1d3bc3c9031b90f5b197a531
xiaobuhuichihuoguo <1136503952@qq.com>
2025-10-22 00:12:24 +0800

    新增唯一属性隐藏id，并将字段与数据库中字段对应起来

--- psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/TransferDetailListDTO.java
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/TransferDetailListDTO.java
@@ -4,38 +4,39 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 @ApiModel("调拨单详细")
 public class TransferDetailListDTO {
-    @ApiModelProperty(value = "查询后数据的id", example = "1")
-    private Integer id;
-    @ApiModelProperty(value = "商品名称", example = "刀具")
-    private String shopName;
-    @ApiModelProperty(value = "商品编号", example = "0003")
-    private Integer shopNumber;
-    @ApiModelProperty(value = "规格标号", example = "DJ-0001")
-    private String Spec;
-    @ApiModelProperty(value = "辅助属性", example = "null")
-    private String Attr;
-    @ApiModelProperty(value = "单位", example = "null")
-    private String Unit;
-    @ApiModelProperty(value = "调出仓库", example = "null")
-    private String OutWarehouse;
-    @ApiModelProperty(value = "调入仓库", example = "null")
-    private String InWarehouse;
-    @ApiModelProperty(value = "批次号", example = "1")
-    private Integer Batch;
+    @ApiModelProperty(value = "隐藏属性唯一id",required = true , example = "1")
+    private int id;
+    @ApiModelProperty(value = "商品名称",required = true, example = "刀具")
+    private String name;
+    @ApiModelProperty(value = "商品编号",required = true, example = "0003")
+    private String number;
+    @ApiModelProperty(value = "规格型号",required = true, example = "DJ-0001")
+    private String spec;
+    @ApiModelProperty(value = "辅助属性",required = true, example = "null")
+    private String attr;
+    @ApiModelProperty(value = "单位", required = true , example = "null")
+    private String unit;
+    @ApiModelProperty(value = "调出仓库",required = true, example = "null")
+    private int warehouse;
+    @ApiModelProperty(value = "调入仓库",required = true, example = "null")
+    private int storehouse;
+    @ApiModelProperty(value = "批次号",required = true, example = "1")
+    private String batch;
     @ApiModelProperty(value = "生产日期", example = "2025-10-18")
-    private LocalDate ProductDate;
-    @ApiModelProperty(value = "成本", example = "2")
-    private Double Cost;
-    @ApiModelProperty(value = "数量", example = "1")
-    private Integer Number;
-    @ApiModelProperty(value = "总成本", example = "2")
-    private Double TotalCost;
+    private int mfd;
+    @ApiModelProperty(value = "成本",required = true,example = "2")
+    private BigDecimal price;
+    @ApiModelProperty(value = "数量",required = true, example = "1")
+    private BigDecimal nums;
+    @ApiModelProperty(value = "总成本",required = true, example = "2")
+    private BigDecimal total;
     @ApiModelProperty(value = "备注信息", example = "null")
-    private String Remark;
+    private String data;
 
 }
