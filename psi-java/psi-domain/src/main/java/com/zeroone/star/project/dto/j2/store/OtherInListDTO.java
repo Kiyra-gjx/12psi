3ba026ce1431fcd120ddd5c28eee51776516c096
chenze5 <chenze5@asiainfo.com>
2025-10-22 09:59:00 +0800

    1. 删除了excel注解 2. 修改了入参和返回参数

--- psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/OtherInListDTO.java
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/OtherInListDTO.java
@@ -22,35 +22,27 @@ public class OtherInListDTO {
     @ApiModelProperty(value = "id", example = "1")
     int id;
 
-    @ExcelProperty(value = "供应商", index = 1)
     @ApiModelProperty(value = "供应商", example = "0")
     int supplier;
 
-    @ExcelProperty(value = "所属组织", index = 0)
     @ApiModelProperty(value = "所属组织", example = "0")
     int frame;
 
-    @ExcelProperty(value = "单据时间", index = 3)
     @ApiModelProperty(value = "单据时间", example = "1760457600")
     int time;
 
-    @ExcelProperty(value = "单据编号", index = 4)
     @ApiModelProperty(value = "单据编号", example = "QTRKD2510150905505")
     String number;
 
-    @ExcelProperty(value = "单据类型", index = 2)
     @ApiModelProperty(value = "单据类型", example = "0")
     int type;
 
-    @ExcelProperty(value = "单据成本", index = 5)
     @ApiModelProperty(value = "单据成本", example = "2.0000")
     float total;
 
-    @ExcelProperty(value = "单据费用", index = 6)
     @ApiModelProperty(value = "单据费用", example = "111.0000")
     float cost;
 
-    @ExcelProperty(value = "关联人员", index = 7)
     @ApiModelProperty(value = "关联人员", example = "1")
     int people;
 
@@ -60,26 +52,21 @@ public class OtherInListDTO {
     @ApiModelProperty(value = "单据附件", example = "[{\"name\":\"5B1CBE37203B5F19F4A745CEEF3C4FAB.png\",\"url\":\"http:\\/\\/localhost:8080\\/static\\/upload\\/entry\\/68f46902ab8d9.png\"}]")
     String file;
 
-    @ExcelProperty(value = "备注信息", index = 12)
     @ApiModelProperty(value = "备注信息", example = "111")
     String data;
 
     @ApiModelProperty(value = "扩展信息", example = "111")
     String more;
 
-    @ExcelProperty(value = "审核状态", index = 8)
     @ApiModelProperty(value = "审核状态", example = "1")
     int examine;
 
-    @ExcelProperty(value = "费用状态", index = 9)
     @ApiModelProperty(value = "费用状态", example = "1")
     int cse;
 
-    @ExcelProperty(value = "核对状态", index = 10)
     @ApiModelProperty(value = "核对状态", example = "1")
     int check;
 
-    @ExcelProperty(value = "制单人", index = 11)
     @ApiModelProperty(value = "制单人", example = "1")
     int user;
 
