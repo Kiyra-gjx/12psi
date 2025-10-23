c77e02913c48a3f75ae7774c83e00b45665421e6
zh <zh@zh>
2025-10-23 20:39:41 +0800

    其他出库单extry及其他出库单详情extryinfo的do实现

---
+++ psi-java/psi-j2-storemanagement/src/main/java/com/zeroone/star/storemanagement/entity/Extry.java
@@ -0,0 +1,47 @@
+package com.zeroone.star.storemanagement.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class Extry {
+//其他出库单
+        private Integer id;
+
+        private Integer customer;
+
+        private int frame;
+
+        private LocalDateTime time;
+
+        private String number;
+
+        private Integer type;
+
+        private BigDecimal total;
+
+        private BigDecimal cost;
+
+        private Integer people;
+
+        private String logistics;
+
+        private String file;
+
+        private String data;
+
+        private String more;
+
+        private Integer examine;
+
+        private Integer cse;
+
+        private Integer check;
+
+        private Integer user;
+
+    }
+
+
