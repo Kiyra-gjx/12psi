68df59b8568e240e64889459614f2c76303796ec
wl <2064525277@qq.com>
2025-10-24 16:09:38 +0800

    增加了attr，warehouse的DO

--- psi-java/psi-j2-storemanagement/src/main/java/com/zeroone/star/storemanagement/entity/Attr.java
+++ psi-java/psi-j2-storemanagement/src/main/java/com/zeroone/star/storemanagement/entity/Attr.java
@@ -6,10 +6,15 @@ import lombok.Data;
 @Data
 public class Attr {
     private String id;
+
     private Integer pit;
+
     private String name;
+
     private Double buy;
+
     private Double sell;
+
     private String code;
 
 }
