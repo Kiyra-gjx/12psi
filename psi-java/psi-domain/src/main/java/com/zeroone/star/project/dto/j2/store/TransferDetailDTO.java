89bcb0f748f8a8a5717a34b1cef6c5e37f7a51b4
Kiyra <Kiyra.gjx@gmail.com>
2025-10-27 23:33:44 +0800

    完成修改调拨单的初步实现，并根据前端进行了数据校验

--- psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/TransferDetailDTO.java
+++ psi-java/psi-domain/src/main/java/com/zeroone/star/project/dto/j2/store/TransferDetailDTO.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @BelongsProject: psi-java
@@ -16,21 +18,78 @@ import java.math.BigDecimal;
 @Data
 @ApiModel("调拨单详情数据对象")
 public class TransferDetailDTO {
-    @ApiModelProperty(value = "调拨单ID", required = true, example = "1")
-    private String id;
+    @ApiModelProperty(value = "单据时间", example = "1760803200")
+    private LocalDateTime time;
 
-    @ApiModelProperty(value = "调出仓库", example = "1号仓库")
-    private String warehouse;
+    @ApiModelProperty(value = "单据编号", example = "DBD2510191240397")
+    private String number;
 
-    @ApiModelProperty(value = "调入仓库", example = "2号仓库")
-    private String storehouse;
+    @ApiModelProperty(value = "商品明细", required = true)
+    private Info info;
 
-    @ApiModelProperty(value = "成本", example = "998.00")
-    private BigDecimal price;
+    @ApiModelProperty(value = "单据明细", required = true)
+    private ClassInfo classInfo;
 
-    @ApiModelProperty(value = "数量", example = "1.00")
-    private BigDecimal nums;
+    @Data
+    @ApiModel("分类信息")
+    public static class ClassInfo {
+        @ApiModelProperty(value = "单据成本", example = "998.00", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private BigDecimal total;
 
-    @ApiModelProperty(value = "备注信息", example = "紧急调拨")
-    private String data;
+        @ApiModelProperty(value = "单据费用", example = "0.00")
+        private BigDecimal cost;
+
+        @ApiModelProperty(value = "关联人员", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private Integer people;
+
+        @ApiModelProperty(value = "物流信息")
+        private String logistics;
+
+        @ApiModelProperty(value = "单据附件", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private String file;
+
+        @ApiModelProperty(value = "备注信息")
+        private String data;
+    }
+
+    @Data
+    @ApiModel("商品信息")
+    public static class Info {
+        @ApiModelProperty(value = "调拨单详情ID", example = "17")
+        private String id;
+
+        @ApiModelProperty(value = "商品名称", example = "男士纯棉圆领T恤", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private String name;
+
+        @ApiModelProperty(value = "商品编号", example = "0001", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private String number;
+
+        @ApiModelProperty(value = "规格型号", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private String spec;
+
+        @ApiModelProperty(value = "辅助属性", example = "经典黑|S", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private String attr;
+
+        @ApiModelProperty(value = "单位", example = "件", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private String unit;
+
+        @ApiModelProperty(value = "调出仓库", example = "2")
+        private Integer warehouse;
+
+        @ApiModelProperty(value = "调入仓库", example = "1")
+        private Integer storehouse;
+
+        @ApiModelProperty(value = "成本", example = "998")
+        private BigDecimal price;
+
+        @ApiModelProperty(value = "数量", example = "1")
+        private BigDecimal nums;
+
+        @ApiModelProperty(value = "总成本", example = "998.00", accessMode = ApiModelProperty.AccessMode.READ_ONLY)
+        private BigDecimal total;
+
+        @ApiModelProperty(value = "备注信息", example = "紧急调拨")
+        private String data;
+
+    }
 }
