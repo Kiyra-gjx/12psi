f8f34c2645306d5af35dbd4be73de7f01cf8e174
wangenqi <86047805@qq.com>
2025-10-29 11:43:39 +0800

    完成了查询批次列表的接口

---
+++ psi-java/psi-j2-storemanagement/src/main/java/com/zeroone/star/storemanagement/service/IBatchListService.java
@@ -0,0 +1,15 @@
+package com.zeroone.star.storemanagement.service;
+
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j2.store.BatchListDTO;
+import com.zeroone.star.project.query.j2.store.BatchQuery;
+import com.zeroone.star.project.vo.JsonVO;
+
+
+public interface IBatchListService {
+
+    /**
+     * 查询批次列表
+     */
+    JsonVO<PageDTO<BatchListDTO>> listBatch(BatchQuery query);
+}
